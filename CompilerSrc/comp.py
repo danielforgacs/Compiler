261b83d7c1700c11ba77d9a2b9f854a8f7430ef8
df@ <@>
2021-05-12 23:51:20 +0100

    clean up

--- CompilerSrc/comp.py
+++ CompilerSrc/comp.py
@@ -29,6 +29,8 @@ is_div = lambda x: x == '/'
 is_paren_l = lambda x: x == '('
 is_paren_r = lambda x: x == ')'
 
+pop_next_token = lambda x: (x[0], x[1:])
+
 
 class Token:
     def __init__(self, type_, value):
@@ -38,6 +40,15 @@ class Token:
         return (self.type_ == other.type_) and (self.value == other.value)
     def __repr__(self):
         return '<Token:{}:{}>'.format(self.type_, self.value)
+
+
+EOF_TOKEN = Token(EOF, EOF)
+ADD_TOKEN = Token(ADD, ADD)
+SUB_TOKEN = Token(SUB, SUB)
+MULT_TOKEN = Token(MULT, MULT)
+DIV_TOKEN = Token(DIV, DIV)
+PAREN_L_TOKEN = Token(PAREN_L, PAREN_L)
+PAREN_R_TOKEN = Token(PAREN_R, PAREN_R)
 
 
 class AST:
@@ -74,17 +85,6 @@ class NodeVisitor:
             return self.visit(node.left) * self.visit(node.right)
         elif node.operator == DIV_TOKEN:
             return self.visit(node.left) / self.visit(node.right)
-
-
-
-
-EOF_TOKEN = Token(EOF, EOF)
-ADD_TOKEN = Token(ADD, ADD)
-SUB_TOKEN = Token(SUB, SUB)
-MULT_TOKEN = Token(MULT, MULT)
-DIV_TOKEN = Token(DIV, DIV)
-PAREN_L_TOKEN = Token(PAREN_L, PAREN_L)
-PAREN_R_TOKEN = Token(PAREN_R, PAREN_R)
 
 
 def find_int_token(src, index):
@@ -135,14 +135,11 @@ def tokenise(source):
     return tokens
 
 
-pop_next_token = lambda x: (x[0], x[1:])
-
-
 def factor(result, tokens):
     token, tokens = pop_next_token(tokens)
     if token.type_ == INTEGER:
         node = Num(token)
-        pass
+
     elif token == PAREN_L_TOKEN:
         token, tokens, node = expression(result, tokens)
         paren_r, tokens = pop_next_token(tokens)
@@ -160,10 +157,12 @@ def term(result, tokens):
 
         if operator == MULT_TOKEN:
             result *= token_right.value
+
         elif operator == DIV_TOKEN:
             result /= token_right.value
 
         node = BinOp(node, operator, node_r)
+
     return Token(INTEGER, result), tokens, node
 
 
@@ -177,10 +176,12 @@ def expression(result, tokens):
 
         if operator == ADD_TOKEN:
             result += token_right.value
+
         elif operator == SUB_TOKEN:
             result -= token_right.value
 
         node = BinOp(node, operator, node_r)
+
     return Token(INTEGER, result), tokens, node
 
 
@@ -188,9 +189,6 @@ def run(source):
     tokens = tokenise(source)
     token, _, node = expression(0, tokens)
 
-    print(NodeVisitor().visit(node))
-
-    # return token.value
     return NodeVisitor().visit(node)
 
 
