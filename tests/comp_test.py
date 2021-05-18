aa3aa53eeb6b99d198817583eed823aaff134196
df@ <@>
2021-05-18 14:33:17 +0100

    test

--- tests/comp_test.py
+++ tests/comp_test.py
@@ -309,3 +309,19 @@ END
 END.
 """
     cmp.program(cmp.tokenise(source))
+
+
+
+def test_program():
+    source = """
+BEGIN
+    BEGIN
+        number := 2;
+        a := number;
+        b := 10 * a + 10 * number / 4;
+        c := a - - b
+    END;
+    x := 11;
+END.
+"""
+    cmp.program(cmp.tokenise(source))
