615fded0916aa2ea4a7e78365aadffebae1a15d6
df@ <@>
2021-05-19 21:27:23 +0100

    added test program

--- CompilerSrc/comp.py
+++ CompilerSrc/comp.py
@@ -411,7 +411,30 @@ def run_program(source):
 
 
 if __name__ == '__main__':
-    code =     """BEGIN{@@@@@@}{@@@@@}END."""
+    code = """
+PROGRAM Part10;
+VAR
+   number     : INTEGER;
+   a, b, c, x : INTEGER;
+   y          : REAL;
+
+BEGIN {Part10}
+   BEGIN
+      number := 2;
+      a := number;
+      b := 10 * a + 10 * number DIV 4;
+      c := a - - b
+   END;
+   x := 11;
+   y := 20 / 7 + 3.14;
+   { writeln('a = ', a); }
+   { writeln('b = ', b); }
+   { writeln('c = ', c); }
+   { writeln('number = ', number); }
+   { writeln('x = ', x); }
+   { writeln('y = ', y); }
+END.  {Part10}
+"""
 
     tokens = tokenise(code)
     # for token in tokens:
