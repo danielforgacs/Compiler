d1b1d707748527f5e5add882dafb02adb732c32a
df@ <@>
2021-05-24 23:22:23 +0100

    deserialise test; error

--- tests/comp_serialise_test.py
+++ tests/comp_serialise_test.py
@@ -8,3 +8,13 @@ def test_Token_serialise():
         'value': 'EOF',
     }
     assert token.asdict == expected
+
+
+
+def test_Token_de_serialise():
+    data = {
+        'type_': 'EOF',
+        'value': 'EOF',
+    }
+    token = comp.Token.from_dict(data=data)
+    assert token == comp.EOF_TOKEN
